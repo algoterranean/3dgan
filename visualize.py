af2744c96cbb5d81f43ea0d5355a3231edad9323
pls <pls@algoterranean.com>
2017-03-25 11:07:32 -0500

    Moved montage/stitching code to common function.

--- visualize.py
+++ visualize.py
@@ -37,35 +37,42 @@ def get_layer(name):
         return None
 
 
+# usage: list(chunks(some_list, chunk_size)) ==> list of lists of that size
+def chunks(l, n):
+    """Yield successive n-sized chunks from l."""
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
+
+        
+def stitch_montage(image_list):
+    num_images = len(image_list)
+    montage_w = ceil(sqrt(num_images))
+    montage_h = int(num_images/montage_w)
+    
+    montage = list(chunks(image_list, montage_w))
+    # fill in any remaining missing images in square montage
+    remaining = montage_w - (num_images - montage_w * montage_h)
+    if remaining < montage_w:
+        dummy_shape = image_list[0].shape
+        # dummy_shape = weights[:,:,:,0].shape if rgb else np.expand_dims(weights[:,:,0,0], 2).shape
+        for _ in range(remaining):
+            montage[-1].append(np.zeros(dummy_shape))
+    return np.vstack([np.hstack(row) for row in montage])
+    
+
+
 # visualize activations for a particular input
 def visualize_activations(layer, input):
     # input is a single image, so put it into batch form for sess.run
     input = np.expand_dims(input, 0)
     x_input = graph.as_graph_element('inputs/x_input').outputs[0]
     activations = sess.run(layer, feed_dict={x_input: input})
-    # calc size of montage
-    num_filters = activations.shape[-1]
-    montage_w = ceil(sqrt(num_filters))
-    montage_h = int(num_filters/montage_w)
-    
-    montage = []
-    row_pos = 0
-    for f_idx in range(num_filters):
-        f = activations[0,:,:,f_idx] * 255.0
-        f = np.expand_dims(f, 2)
-        if row_pos % montage_w == 0:
-            montage.append([f])
-        else:
-            montage[-1].append(f)
-        row_pos += 1
 
-    # fill in any remaining missing images in square montage
-    remaining = montage_w - (num_filters - montage_w * montage_h)
-    if remaining < montage_w:
-        dummy_shape = np.expand_dims(activations[0,:,:,0], 2).shape
-        for _ in range(remaining):
-            montage[-1].append(np.zeros(dummy_shape))
-    return np.vstack([np.hstack(row) for row in montage])
+    image_list = []
+    for f_idx in range(activations.shape[-1]):
+        f = activations[0,:,:,f_idx] * 255.0
+        image_list.append(np.expand_dims(f, 2))
+    return stitch_montage(image_list)
 
 
 def visualize_all_activations(layers, input):
@@ -87,39 +94,16 @@ def visualize_weights(var):
     weights = sess.run(var)
     rgb = weights.shape[-2] == 3 # should output be rgb or grayscale?
     num_filters = weights.shape[-1] if rgb else weights.shape[-1] * weights.shape[-2]
-    montage_w = ceil(sqrt(num_filters))
-    montage_h = int(num_filters/montage_w)
 
-    montage = []
-    row_pos = 0
+    image_list = []
     for f_idx in range(weights.shape[-1]):
-        # rgb
         if rgb:
-            f = weights[:,:,:,f_idx] * 255.0
-            if row_pos % montage_w == 0:
-                montage.append([f])
-            else:
-                montage[-1].append(f)
-            row_pos += 1
-        # do each filter channel individually as grayscale
+            image_list.append(weights[:,:,:,f_idx]*255.0)
         else:
-            for f2_idx in range(weights.shape[-2]):
-                f = weights[:,:,f2_idx,f_idx] * 255.0
-                f = np.expand_dims(f, 2)
-                if row_pos % montage_w == 0:
-                    montage.append([f])
-                else:
-                    montage[-1].append(f)
-                row_pos += 1
-
-    # fill in any remaining missing images in square montage
-    remaining = montage_w - (num_filters - montage_w * montage_h)
-    if remaining < montage_w:
-        dummy_shape = weights[:,:,:,0].shape if rgb else np.expand_dims(weights[:,:,0,0], 2).shape
-        for _ in range(remaining):
-            montage[-1].append(np.zeros(dummy_shape))
-
-    return np.vstack([np.hstack(row) for row in montage])
+            for f_idx2 in range(weights.shape[-2]):
+                f = weights[:,:,f_idx2,f_idx] * 255.0
+                image_list.append(np.expand_dims(f, 2))
+    return stitch_montage(image_list)
 
 
 def visualize_all_weights(weights):
@@ -129,31 +113,34 @@ def visualize_all_weights(weights):
 # weight_vars = [v for v in tf.trainable_variables() if len(v.get_shape()) == 4]
 # results = visualize_all_weights(weight_vars)
 # for i in range(len(results)):
+#     print('results', i, results[i].shape)
 #     cv2.imwrite('weights_' + str(i) + '.png', results[i])
 
 
 
 
+# visualize image that most activates a filter via gradient ascent
 
 
-    
-
-
-# # # visualize image that most activates a filter via gradient ascent
+# # # how to use:
+# # layers = tf.get_collection(key='layers')
 # input_img = graph.as_graph_element('inputs/x_input').outputs[0]
 # layer_output = graph.as_graph_element('outputs/encoder/Layer.Encoder.64').outputs[0]
+# visualize_bestfit_image(layer_output)
 
-# for idx in range(layer_output.get_shape()[-1]):
-#     loss = tf.reduce_mean(layer_output[:,:,:,idx])
-#     grads = tf.gradients(loss, input_img)[0]
-#     input_img_data = np.random.random([1, 64, 64, 3])
 
-#     for n in range(20):
-#         loss_value, grads_value = sess.run([loss, grads], feed_dict={input_img: input_img_data})
-#         print('grads:',grads_value.shape)
-#         input_img_data += grads_value
+# def visualize_bestfit_image(layer):
+#     for idx in range(layer.get_shape()[-1]):
+#         input_img_data = np.random.random([1, 64, 64, 3])
+#         loss = tf.reduce_mean(layer[:,:,:,idx])
+#         grads = tf.gradients(loss, input_img)[0]
 
-#     input_img_data = np.squeeze(input_img_data)
+#         for n in range(20):
+#             loss_value, grads_value = sess.run([loss, grads], feed_dict={input_img: input_img_data})
+#             print('grads:',grads_value.shape)
+#             input_img_data += grads_value
+
+#         input_img_data = np.squeeze(input_img_data)
 #     cv2.imwrite('grad_ascent_{}.png'.format(idx), input_img_data * 255.0)
 
 
